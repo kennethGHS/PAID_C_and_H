61a6955aaa5455a137e95c15bf8393e2611b6f6e
KennethGHS <khken21@gmail.com>
2021-09-11 20:58:41 -0600

    Add test file

---
+++ Catalog 1/Histograms/tests.py
@@ -0,0 +1,7 @@
+from enlarge_histogram import *
+from reduce_histogram import *
+from histogram import *
+
+reduce_histogram(120,120,filename="./actor.jpg")
+enlarge_histogram(filename="./sydney.jpg")   
+calculate_histogram(filename="./Test1.jpg")
