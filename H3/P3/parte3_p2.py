36ffd5642b1d418c4eff6bbdb8f3e2e078a3c348
Isaac Porras <isaacpope@estudiantec.cr>
2021-11-13 14:18:51 -0600

    Add finals commits

--- H3/P3/parte3_p2.py
+++ H3/P3/parte3_p2.py
@@ -2,6 +2,15 @@ import numpy as np
 import cv2 as cv
 
 
+'''
+Algoritmo para detectar multiples circulos en imagenes mediante el metodo de 
+la transformada de Hugh. Encuentre la explicación en la infografia que se encuentra dentro
+de esta carpeta
+-Entradas: imagen 1, imagen 2, imagen 3
+- Salidas: imagen 1,2 y 3 con circulos detectados
+
+'''
+#Detección de circulos de la primera imagen
 img = cv.imread('imagen1.png')
 output = img.copy()
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -12,6 +21,8 @@ detected_circles = np.uint16(np.around(circles))
 for (x, y ,r) in detected_circles[0, :]:
     cv.circle(output, (x, y), r, (0, 0, 0), 3)
     cv.circle(output, (x, y), 2, (0, 255, 255), 3)
+
+#Detección de circulos de la segunda imagen
 
 img2 = cv.imread('imagen2.png')
 output2 = img2.copy()
@@ -24,7 +35,7 @@ for (x, y ,r) in detected_circles2[0, :]:
     cv.circle(output2, (x, y), r, (0, 0, 0), 3)
     cv.circle(output2, (x, y), 2, (0, 255, 255), 3)
 
-
+#Detección de circulos de la tercera imagen
 img3 = cv.imread('imagen3.png')
 output3 = img3.copy()
 gray3 = cv.cvtColor(img3, cv.COLOR_BGR2GRAY)
