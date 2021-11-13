c22157cae6b5f4b120f8aff7973ac4cc69371b46
Isaac Porras <isaacpope@estudiantec.cr>
2021-11-13 11:14:17 -0600

    Add final documentation

--- Catalog3/ImageSegmentation/otsu.m
+++ Catalog3/ImageSegmentation/otsu.m
@@ -12,17 +12,25 @@ title('Imagen Original');
 subplot(2,2,2);
 imhist(A);
 title('Histograma de la imagen original');
-
+%Metodo del Umbral Global basico el cual toma como entrada una imagen y la segmenta en dos tonalidades de gris
+%Entradas: Una imagen A
+%Salidas: imagen segmentada en dos tonalidades.
+%Paso 1:Seleccionamos un valor inicial de umbral
 T = 165;
+%Paso 2: Seleccionamos un valor de iteraciones
 iter = 15;
 
 for k = 1:iter
+  %Dividimos la imagen en dos bloques de intensidades mayores y menores a Tk
   I1 = (A>T);
   I2 = (A<=T);
+  
   B1 = A.*I1;
   B2 = A.*I2;
+  %Calculamos el promedio de intensidad de la imagen original
   m1 = sum(sum(B1))/sum(sum(I1));
   m2 = sum(sum(B2))/sum(sum(I2));
+  %Actualizamos el nuevo umbral
   T = 0.5 *(m1 + m2);
  endfor
  
